6410f0874ffe5418a45c3620e89f28facd6e6692
Jack <jack@top.com>
2020-10-09 18:35:54 +0800

    修改购物车跳转

--- lib/CarDetailVC.dart
+++ lib/CarDetailVC.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// 购物车页面
 class CarListDetailVC extends StatelessWidget {
   const CarListDetailVC({Key key}) : super(key: key);
 
@@ -14,8 +15,31 @@ class CarListDetailVC extends StatelessWidget {
               child: Text("back"),
               onPressed: () {
                 print('返回上级目录');
-                Navigator.pop(context);
+                // Navigator.pop(context);
+                // ShowAlertView();
               })),
+    );
+  }
+}
+
+//模态视图样式
+class ShowAlertView extends StatelessWidget {
+  const ShowAlertView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text(
+          '这个是模态视图',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+            fontSize: 24.0,
+          ),
+        ),
+      ),
     );
   }
 }
